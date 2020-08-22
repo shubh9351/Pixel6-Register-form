fb83593485f02d5e8f7fc289dfb901a6a07b7dcb
SHUBHAM PRABHAKAR JIOTODE <65721216+shubh9351@users.noreply.github.com>
2020-08-22 09:56:50 +0530

    Add files via upload
    modified validations

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 function checkName() {
   var user = document.getElementById('fName').value;
   var wordCount = user.split(" ").length;
-  if (user === " ") {
+  var pattern =/^[a-zA-z ]+$/ ;
+
+  if (user.length === 0) {
     document.getElementById('fullName').innerHTML = " ** Please fill the full name.";
     return false;
   }
-  if (!isNaN(user)) {
-    document.getElementById('fullName').innerHTML = " ** only characters are allowed";
+  if(!user.match(pattern)){
+    document.getElementById('fullName').innerHTML = " ** No special characters are allowed";
     return false;
   }
   if (user.length < 4) {
@@ -25,8 +27,8 @@ function checkName() {
 // adding validation to Email
 function checkEmail() {
   var emails = document.getElementById('emailId').value;
-  if (emails === " ") {
-    document.getElementById('emails').innerHTML = " ** Please fill the email id ";
+  if (emails.length === 0) {
+    document.getElementById('emails').innerHTML = " ** Please fill the Email id ";
     return false;
   }
   if (emails.indexOf('@') <= 0) {
@@ -49,41 +51,278 @@ function checkPhone() {
   var provider = mobileNumber.slice(0, 3);
   var state = mobileNumber.slice(3, 6);
   var remain = mobileNumber.slice(6, 10);
-  var newNum = "(" + provider + ")-" + state + "-" + remain;
-  if (mobileNumber === " ") {
-    document.getElementById('phone').innerHTML = " ** Please fill the Phone number";
-    return false;
-  }
-  if(isNaN(mobileNumber)) {
-    document.getElementById('phone').innerHTML = " ** Special characters not allowed";
-    return false;
-  }
-  if (mobileNumber.length != 10) {
-    document.getElementById('phone').innerHTML = " ** Mobile Number must be 10 digits only";
-    return false;
-  } else if (provider > 621 && provider <= 800) {
+  var firstNum=mobileNumber.slice(1,2);
+  var provider1= "("+provider +")";
+  var state1="-"+state;
+  var remain1= "-"+remain;
+
+
+  if (provider > 621 && provider <= 800 && state>000 && state<=100 && remain>0000 && remain<=9999) {
     document.getElementById('phone').classList.remove("warning");
     document.getElementById('phone').classList.add("Jio");
-    document.getElementById('phone').innerHTML = "Reliance Jio, Maharashtra";
-    document.getElementById('phoneNum').value = newNum;
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Andra Pradesh."
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
     return false;
   }
-  if (provider > 801 && provider <= 920) {
+  if (provider > 621 && provider <= 800 && state>101 && state<=150 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Assam."
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>151 && state<=200 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Arunachal Pradesh."
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>201 && state<=250 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Assam."
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>251 && state<=300 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Bihar";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>301 && state<=350 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Chattisgarh";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>351 && state<=400 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Goa";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }if (provider > 621 && provider <= 800 && state>401 && state<=450 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Gujarat";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>851 && state<=900 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Haryana";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>901 && state<=950 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Goa";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+
+  if (provider > 621 && provider <= 800 && state>450 && state<=850 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Maharashtra."
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 621 && provider <= 800 && state>950 && state<=999 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Jio");
+    document.getElementById('phone').innerHTML = "Reliance Jio,";
+    document.getElementById("state").innerHTML="Jharkhand";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>000 && state<=050 && remain>0000 && remain<=9999) {
     document.getElementById('phone').classList.remove("warning");
     document.getElementById('phone').classList.add("Idea");
-    document.getElementById('phone').innerHTML = "idea,Maharashtra";
-    document.getElementById('phoneNum').value = newNum;
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="kerala";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
     return false;
   }
-  if (provider > 921 && provider <= 999) {
+  if (provider > 801 && provider <= 920 && state>051 && state<=150 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="Delhi";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>150 && state<=250 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="Rajastan";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>251 && state<=300 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="Jharkhand";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>450 && state<=750 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="Maharashtra";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>751 && state<=900 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "Idea,";
+    document.getElementById("state").innerHTML="Uttar Pradesh";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>301 && state<=350 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "idea,";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    document.getElementById("state").innerHTML="West-Bengal";
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>351 && state<=400 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "idea,";
+    document.getElementById("state").innerHTML="Jharkhand";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>400 && state<=450 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "idea,";
+    document.getElementById("state").innerHTML="Chandigarh";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if (provider > 801 && provider <= 920 && state>450 && state<=850 && remain>0000 && remain<=9999) {
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Idea");
+    document.getElementById('phone').innerHTML = "idea,";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if(provider > 921 && provider <= 999 && state>000 && state<=100 && remain>0000 && remain<=9999){
     document.getElementById('phone').classList.remove("warning");
     document.getElementById('phone').classList.add("Vodafone");
-    document.getElementById('phone').innerHTML = "Vodafone ,Maharashtra";
-    document.getElementById('phoneNum').value = newNum;
-    return false;
-  } else {
-    document.getElementById('phone').innerHTML = "invalid";
-    document.getElementById('phoneNum').value = newNum;
+    document.getElementById('phone').innerHTML = "Vodafone,";
+    document.getElementById("state").innerHTML="Delhi"
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
     return false;
   }
+  if(provider > 921 && provider <= 999 && state>100 && state<=200 && remain>0000 && remain<=9999){
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Vodafone");
+    document.getElementById('phone').innerHTML = "Vodafone,";
+    document.getElementById("state").innerHTML="Haryana"
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+  }
+  if(provider > 921 && provider <= 999 && state>201 && state<=250 && remain>0000 && remain<=9999){
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Vodafone");
+    document.getElementById('phone').innerHTML = "Vodafone,";
+    document.getElementById("state").innerHTML="Panjab"
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+    }
+    if(provider > 921 && provider <= 999 && state>251 && state<=350 && remain>0000 && remain<=9999){
+      document.getElementById('phone').classList.remove("warning");
+      document.getElementById('phone').classList.add("Vodafone");
+      document.getElementById('phone').innerHTML = "Vodafone,";
+      document.getElementById("state").innerHTML="TamilNadu"
+      document.getElementById('phoneNum').value = provider1+state1+remain1;
+      return false;
+      }
+      if(provider > 921 && provider <= 999 && state>351 && state<=450 && remain>0000 && remain<=9999){
+        document.getElementById('phone').classList.remove("warning");
+        document.getElementById('phone').classList.add("Vodafone");
+        document.getElementById('phone').innerHTML = "Vodafone,";
+        document.getElementById("state").innerHTML="Goa"
+        document.getElementById('phoneNum').value = provider1+state1+remain1;
+        return false;
+        }
+        if(provider > 921 && provider <= 999 && state>450 && state<=750 && remain>0000 && remain<=9999){
+          document.getElementById('phone').classList.remove("warning");
+          document.getElementById('phone').classList.add("Vodafone");
+          document.getElementById('phone').innerHTML = "Vodafone,";
+          document.getElementById("state").innerHTML="Maharashtra"
+          document.getElementById('phoneNum').value = provider1+state1+remain1;
+          return false;
+          }
+          if(provider > 921 && provider <= 999 && state>750 && state<=800 && remain>0000 && remain<=9999){
+            document.getElementById('phone').classList.remove("warning");
+            document.getElementById('phone').classList.add("Vodafone");
+            document.getElementById('phone').innerHTML = "Vodafone,";
+            document.getElementById("state").innerHTML="Madhya Pradesh";
+            document.getElementById('phoneNum').value = provider1+state1+remain1;
+            return false;
+            }
+            if(provider > 921 && provider <= 999 && state>801 && state<=900 && remain>0000 && remain<=9999){
+              document.getElementById('phone').classList.remove("warning");
+              document.getElementById('phone').classList.add("Vodafone");
+              document.getElementById('phone').innerHTML = "Vodafone,";
+              document.getElementById("state").innerHTML="Maharashtra";
+              document.getElementById('phoneNum').value = provider1+state1+remain1;
+              return false;
+              }
+  if(provider > 921 && provider <= 999 && state>901 && state<=999 && remain>0000 && remain<=9999){
+    document.getElementById('phone').classList.remove("warning");
+    document.getElementById('phone').classList.add("Vodafone");
+    document.getElementById('phone').innerHTML = "Vodafone,";
+    document.getElementById("state").innerHTML="Telengana";
+    document.getElementById('phoneNum').value = provider1+state1+remain1;
+    return false;
+    }
+  if (mobileNumber.length != 14){
+    document.getElementById('errMsg').innerHTML = " ** Mobile Number must be 10 digits only";
+    return false;
+  }
+  if(firstNum!=9 && firstNum!=8 && firstNum!=7 && firstNum!=6){
+      document.getElementById('errMsg').innerHTML = "** Invalid number";
+      return false;
+  }
+else{
+    document.getElementById('errMsg').innerHTML = " ";
+    return false;
+    }
 }
+
+function restrictAlphabets(e){
+       var x = e.which || e.keycode;
+   	if((x>=48 && x<=57)){
+   		return true;
+   	}else{
+   		return false;
+    }
+  }
--- validate.js
+++ validate.js
@@ -1,13 +1,11 @@
-// creating function for otp grneration and which will validate form also,
-// if user submit without entring important filled
-function otpGeneration(num) {
+
+ function myFun(){
   var name = localStorage.getItem("Name");
   var phone = localStorage.getItem("Number");
-  var otpVal = Math.floor(Math.random() * 1000 + 1000);
-  localStorage.setItem("otpVal", otpVal);
-  console.log(otpVal);
-  document.getElementById("userMsg").innerHTML = "Dear " + name + ", Thank you for your enquiry.";
-  document.getElementById("pinMsg").innerHTML = "A 4 digit verification number has been sent to your phone number: " + phone;
+  var oneTimePass=localStorage.getItem("otpVal");
+  document.getElementById("userMsg").innerHTML =  name ;
+  document.getElementById("pinMsg").innerHTML =  phone;
+  document.getElementById("otpVal").innerHTML = oneTimePass ;
   return;
 }
 
@@ -17,25 +15,46 @@ function otpGeneration(num) {
 function otpValidation() {
   var userOtp = document.getElementById("otpGen").value;
   var genOtp = localStorage.getItem("otpVal");
-  console.log(userOtp);
-  if (userOtp != genOtp){
+  console.log(genOtp);
+  if(userOtp != genOtp){
     document.getElementById("valFailure").innerHTML = " **Invalid OTP,Please enter again (only 3 Attempts left).";
     document.getElementById("valFailure").classList.add("valSuccess");
     var count = 0;
     var countButton = document.getElementById("countButton");
-    countButton.onclick = function() {
+    countButton.onclick = function(){
       count++;
       var newCount = count;
-      if (newCount === 1) {
-        document.getElementById("valFailure").innerHTML = " **Invalid OTP,Please enter again (only 2 Attempts left).";
-      }else if(newCount === 2 && userOtp!=genOtp){
-        document.getElementById("valFailure").innerHTML = " **Invalid OTP,Please enter again (only 1 Attempts left).";
-      }else if(newCount === 3 && userOtp!=genOtp){
-        location.replace("failure.html");
-      }else{
+      var newOtp = document.getElementById("otpGen").value;
+      console.log(newCount);
+      console.log(newOtp);
+      if(newCount == 1 && newOtp == genOtp){
         document.getElementById("valFailure").innerHTML = " Validation Successfull";
         document.getElementById("valFailure").classList.add("valSuccess");
         location.replace("http://pixel6.co/");
+        return false;
+      }
+      if(newCount== 1 && newOtp != genOtp){
+          document.getElementById("valFailure").innerHTML = " **Invalid OTP,Please enter again (only 2 Attempts left).";
+          return false;
+      }
+      if(newCount == 2 && newOtp == genOtp){
+        document.getElementById("valFailure").innerHTML = " Validation Successfull";
+        document.getElementById("valFailure").classList.add("valSuccess");
+        location.replace("http://pixel6.co/");
+        return false;
+      }
+      if( newCount==2 && newOtp!=genOtp){
+        document.getElementById("valFailure").innerHTML = " **Invalid OTP,Please enter again (only 1 Attempts left).";
+        return false;
+      }
+      if(newCount == 3 && newOtp == genOtp){
+         document.getElementById("valFailure").innerHTML = " Validation Successfull";
+         document.getElementById("valFailure").classList.add("valSuccess");
+         location.replace("http://pixel6.co/");
+         return false;
+       }
+      if(newCount ==3 && newOtp != genOtp){
+        location.replace("http://pixel6.co/failur");
         return false;
       }
   }
